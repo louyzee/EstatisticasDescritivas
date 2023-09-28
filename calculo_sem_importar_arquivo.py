c9f4954dac8ae53d65bcda28aa47de1c6a418b89
louyzee <146354914+louyzee@users.noreply.github.com>
2023-09-28 18:09:59 -0300

    Update calculo_sem_importar_arquivo.py

--- calculo_sem_importar_arquivo.py
+++ calculo_sem_importar_arquivo.py
@@ -1,18 +1,15 @@
 import pandas as pd
-import numpy as np
-
-#data = pd.read_csv('salarios.csv')
 
 # dataframe = pd.DataFrame()
 # series = pd.Series()
-# serie = pd.Series(dados_df['Peso'])
 # dataframe.describe() - mostra estatísticas descritivas
-dados = {'Nome': ['Otavio', 'Sandra', 'Ana', 'Francisco'],
-         'NotaAluno': [1, 2, 2, 3],
-         'Sexo': ['Masculino', 'Feminino', 'Feminino', 'Masculino'],
-         'Idade': [18, 19, 18, 22],
-         'Peso': [98, 66, 102, 76],
-         'Altura': [1.780, 1.680, 1.570, 1.590]
+
+dados = {'Nome': ['Otavio', 'Sandra', 'Ana', 'Francisco', 'Rafael'],
+         'NotaDoAluno': [1, 2, 2, 3, 3],
+         'Sexo': ['Masculino', 'Feminino', 'Feminino', 'Masculino', 'Masculino'],
+         'Idade': [18, 19, 18, 22, 18],
+         'Peso': [98, 66, 102, 76, 90],
+         'Altura': [1.780, 1.680, 1.570, 1.590, 1.630]
         }
 
 dados_df = pd.DataFrame(dados)
@@ -24,38 +21,39 @@ dadosNum = {
 }
 dadosNum_df = pd.DataFrame(dadosNum)
 
+#Exibição dos dados a serem utilizados
+print('Dados utilizados:')
+print(dados_df,'\n')
+
 # Média
-media = dados_df.mean()
+media = dadosNum_df.mean()
 print('Média: ')
 print(media,'\n')
 
-
 # Mediana
-mediana = dados_df.median()
+mediana = dadosNum_df.median()
 print('Mediana: ')
 print(mediana,'\n')
 
-
 # Moda
-moda = dados_df.mode()
+moda = dados_df.drop(['Nome'],axis=1).mode()
 print('Moda: ')
 print(moda,'\n')
 
 # Quartil
-quartil = dados_df.quantile()
+quartil = dadosNum_df.quantile()
 print('Quartil: ')
 print(quartil,'\n')
 
-
 # Variância
-variancia = dados_df.var()
+variancia = dadosNum_df.var()
 print('Variância: ')
 print(variancia,'\n')
 
 # Desvio Padão
-desvio = dados_df.std()
+desvio = dadosNum_df.std()
 print('Desvio Padrão: ')
 print(desvio,'\n')
 
 print('Outros dados:')
-print(dados_df.describe())
+print(dados_df.describe(),'\n')
